9ab78038f1fc8cffd5d83520ae9398a154a80562
Marc Daffern <marcdaffern@Marcs-MacBook-Air.local>
2026-09-22 22:12:17 +0100

    talking to db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/test") {
+      const recipe = await env.DB
+        .prepare("SELECT * FROM recipes WHERE slug = ?")
+        .bind("test-recipe")
+        .first();
+
+      return Response.json(recipe);
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
